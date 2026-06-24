b0deda1e0ce6a8328f748f3d84ceb03f4967af11
hrittik702 <mmhrittik@gmail.com>
2026-06-24 13:45:48 +0530

    feat : play/pause song

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-// Initialize Lenis
-const lenis = new Lenis();
+const playBtn = document.getElementById("play");
+const pauseBtn = document.getElementById("pause");
+const audioPlayer = document.getElementById("audioPlayer");
 
-// Use requestAnimationFrame to continuously update the scroll
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
+// Hide pause icon initially
+pauseBtn.style.display = "none";
 
-requestAnimationFrame(raf);
+playBtn.addEventListener("click", () => {
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "inline-block";
+  } else {
+    audioPlayer.pause();
+    playBtn.style.display = "inline-block";
+    pauseBtn.style.display = "none";
+  }
+});
+
+pauseBtn.addEventListener("click", () => {
+  audioPlayer.pause();
+  pauseBtn.style.display = "none";
+  playBtn.style.display = "inline-block";
+});
